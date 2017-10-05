d439665562690f689837a828fd167dc05c480f3c
Leef_ <zhenyuan.ma.polyu@gmail.com>
2017-10-05 20:25:02 +0800

    repair plugin

--- app/data/index.js
+++ app/data/index.js
@@ -24,6 +24,26 @@ define((require, exports, module) => {
       }
     })
   }
+  const createRepairPlugin = (store) => {
+    store.subscribe((mutation, state) => {
+      function updateRepairTime (slotNo) {
+        let slot = _.get(state, ['repair', slotNo])
+        let sword = _(state.repair.slot)
+          .values()
+          .map(o => o.sword_serial_id)
+          .filter(_.isNumber)
+          .map(o => _.get(state, ['swords', 'serial', o, 'name']))
+          .value()
+        let time = _(state.repair.slot).finished_at.values()
+        console.log(name + "手入开始，将于" + time + "完成")
+      }
+      if (mutation.type === 'repair/updateRepair') {
+        let { slotNo } = mutation.payload
+        updateRepairTime(slotNo)
+      }
+    })
+  }
+
   return new Vuex.Store({
     namespaced: true,
     state () {
@@ -51,6 +71,6 @@ define((require, exports, module) => {
       battle_player: require('./battle_player'),
       sally: require('./sally')
     },
-    plugins: [createPartyLevelPlugin]
+    plugins: [createPartyLevelPlugin, createRepairPlugin]
   })
 })
