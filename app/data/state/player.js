9c0a4906b9e425cf6f2c8f793b7b95e7a00c79eb
hitomi <huahuohe@foxmail.com>
2017-10-06 17:31:47 +0800

    notice animation

--- app/data/state/player.js
+++ app/data/state/player.js
@@ -8,6 +8,7 @@ define((require, exports, module) => {
     mutations: {
       updatePlayer (state, payload) {
         let {updateData} = payload
+        if (state.name && !updateData.name) updateData = state.name
         mergeModel(state, updateData)
       }
     }
