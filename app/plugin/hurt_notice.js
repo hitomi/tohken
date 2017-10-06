90de473f7983959cc59d0820bcdbaad720de1071
ZhenyuanMA <zhenyuan.ma.polyu@gmail.com>
2017-10-06 15:55:23 +0800

    hurt notice

---
+++ app/plugin/hurt_notice.js
@@ -0,0 +1,9 @@
+define((require, exports, module) => {
+    const SwordMasterData = require('data/SwordMaster1507123959937')
+    return (store) => {
+      store.subscribe((mutation, state) => {
+        
+      })
+    }
+  })
+  
