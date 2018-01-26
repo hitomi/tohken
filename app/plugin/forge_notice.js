eaf61edb21d35d1e55375e9fd0efd168fc86fca0
nmx14 <nmx14@mails.tsinghua.edu.cn>
2018-01-26 08:48:49 +0800

    bug fixed 'forge notice'

--- app/plugin/forge_notice.js
+++ app/plugin/forge_notice.js
@@ -6,7 +6,7 @@ define((require, exports, module) => {
         if (mutation.type === 'forge/updateForge') {
           let { updateData } = mutation.payload
           let getSwordId = updateData.sword_id
-          let time = moment(mutation.payload.updateData.finished_at)
+          let time = moment(parseValues(mutation.payload.updateData.finished_at))
           let swordName = _.get(TRHMasterData.getMasterData('Sword'), [getSwordId, 'name'], '无')
           let logId = `${updateData.slot_no}#${time.unix()}`
           store.commit('log/addForgeLog', {
@@ -16,7 +16,7 @@ define((require, exports, module) => {
           if (getSwordId) {
             store.dispatch('notice/addNotice', {
               title: `锻刀剧透： ${swordName}`,
-              message: `结束时间：${time.subtract(1, 'h').format('hh:mm:ss')}`,
+              message: `结束时间：${time.format('hh:mm:ss')}`,
               context: time.isBefore() ? '已经結束了呦！' : '请耐心等待哟（或者拍个加速？）',
               tag: getSwordId,
               renotify: true,
@@ -26,7 +26,7 @@ define((require, exports, module) => {
           } else {
             store.dispatch('notice/addNotice', {
               title: `锻刀剧透等待中`,
-              message: `结束时间：${time.subtract(1, 'h').format('hh:mm:ss')}`,
+              message: `结束时间：${time.format('hh:mm:ss')}`,
               context: '需要重新进入锻刀页面才能看到呦',
               tag: getSwordId,
               renotify: true,
