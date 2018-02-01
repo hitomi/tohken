25d87fcc02b40743ea0cfe7213f3b631093e0711
nmx14 <nmx14@mails.tsinghua.edu.cn>
2018-02-01 12:02:18 +0800

    equip destroy notice

--- app/plugin/hurt_notice.js
+++ app/plugin/hurt_notice.js
@@ -56,11 +56,70 @@ define((require, exports, module) => {
             .values()
             .value()
           let getSwordId = _.get(updateData, ['result', 'get_sword_id'])
+          let playerEquips = _(_.get(updateData, ['player', 'party']))
+            .values()
+            .keyBy(o => o.serial_id)
+            .mapValues((v, k) => {
+              let equip1 = null
+              let equip2 = null
+              let equip3 = null
+              if(v.init_soldier1>0 && v.soldier1 == 0){
+                equip1 = v.equip_id1
+              }
+              if(v.init_soldier2>0 && v.soldier2 == 0){
+                equip2 = v.equip_id2
+              }
+              if(v.init_soldier3>0 && v.soldier3 == 0){
+                equip3 = v.equip_id3
+              }
+              return {
+                serial_id: v.serial_id,
+                equip: {
+                  1: equip1,
+                  2: equip2,
+                  3: equip3
+                }
+              }
+            })
+            .filter(o => {
+              if(o.equip[1] != null || o.equip[2] != null || o.equip[3] != null)
+                return o
+            })
+            .mapValues((v,k) => {
+              let sword = _.get(state, ['swords', 'serial', v.serial_id], {})
+              let equipstring = ''
+              if(v.equip[1]!=null)
+                equipstring += _.get(TRHMasterData.getMasterData('Equip'), [v.equip[1], 'name'], '-') + ' '
+              if(v.equip[2]!=null)
+                equipstring += _.get(TRHMasterData.getMasterData('Equip'), [v.equip[2], 'name'], '-') + ' '
+              if(v.equip[3]!=null)
+                equipstring += _.get(TRHMasterData.getMasterData('Equip'), [v.equip[3], 'name'], '-') + ' '
+              equipstring += '\n'
+              return {
+                serial_id: v.serial_id,
+                name: sword.name,
+                equips: equipstring
+              }
+            })
+            .values()
+            .value()
           if (playerParty.length) {
             let swordName = _.get(TRHMasterData.getMasterData('Sword'), [getSwordId, 'name'], '无')
+            if (swordName)
             store.dispatch('notice/addNotice', {
               title: `战斗报告`,
-              message: _.map(playerParty, o => `[${o.battleStatusText}] ${o.name} HP -${o.hp}`).join('<br>'),
+              message: _.map(playerEquips, o => `[刀装破坏] ${o.name} - ${o.equips}`).join('<br>')+'<br>'+_.map(playerParty, o => `[${o.battleStatusText}] ${o.name} HP -${o.hp}`).join('<br>'),
+              context: `掉落：${swordName}！`,
+              swordBaseId: getSwordId,
+              icon: `static/sword/${getSwordId}.png`,
+            })
+          }
+          else if (playerEquips.length) {
+            let swordName = _.get(TRHMasterData.getMasterData('Sword'), [getSwordId, 'name'], '无')
+            if (swordName)
+            store.dispatch('notice/addNotice', {
+              title: `战斗报告`,
+              message: _.map(playerEquips, o => `[刀装破坏] ${o.name} - ${o.equips}`).join('<br>'),
               context: `掉落：${swordName}！`,
               swordBaseId: getSwordId,
               icon: `static/sword/${getSwordId}.png`,
