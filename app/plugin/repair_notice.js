568e9c1a16c45512ec21e3841f405a66e53ccac1
nmx14 <nmx14@mails.tsinghua.edu.cn>
2018-02-19 23:51:43 +0800

    sword icon

--- app/plugin/repair_notice.js
+++ app/plugin/repair_notice.js
@@ -9,10 +9,10 @@ define((require, exports, module) => {
             title: `手入刀剑：${sword.name} `,
             message: `结束时间：${moment(parseValues(mutation.payload.updateData.finished_at)).format('MM/DD HH:mm:ss')}`,
             context: moment(parseValues(mutation.payload.updateData.finished_at)).isBefore() ? '已经結束了呦！' : '请耐心等待哟（或者拍个加速？）',
-            tag: sword.baseId,
+            tag: sword.sword_id,
             renotify: true,
-            swordBaseId: sword.baseId,
-            icon: `static/sword/${sword.baseId}.png`
+            swordBaseId: sword.sword_id,
+            icon: `static/sword/${sword.sword_id}.png`
           })
         }
       }
