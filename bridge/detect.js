ccae6452c15d0bbec075079fc861323cbad455d4
nmx14 <nmx14@mails.tsinghua.edu.cn>
2018-04-24 18:00:52 +0800

    1.1.6

--- bridge/detect.js
+++ bridge/detect.js
@@ -4,6 +4,6 @@ chrome.runtime.sendMessage({
   message: {
     title: '欢迎使用婶婶忙~',
     message: '请在游戏运行之前通过在页面空白处右键审查元素（Inspect），或按下F12键打开开发者工具',
-    context: '构建版本：v1.1.5'
+    context: '构建版本：v1.1.6'
   }
 })
